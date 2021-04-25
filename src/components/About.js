eb08434e81644e091fd8fcaa9a36895117c44d77
antonpodkur <anton.podkur@gmail.com>
2021-04-26 00:16:52 +0300

    Added some responsive ui

--- src/components/About.js
+++ src/components/About.js
@@ -23,14 +23,14 @@ export default function About() {
     if(!author) return(<div>Loading...</div>)
 
     return (
-        <main className="bg-gradient-to-b from-purple-400 via-indigo-300 to-pink-200 min-h-screen p-12">
+        <main className="bg-gradient-to-b from-purple-400 via-indigo-300 to-pink-200 min-h-screen sm:p-12">
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-white rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={author.image.asset.url} alt={author.image.url} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" />
-                    <div className="text-lg flex flex-col justify-center relative mx-auto">
-                        <h1 className=" cursive text-6xl text-black mb-4">
+                <section className="bg-white flex rounded-lg shadow-2xl flex-col md:flex-row text-center p-5 sm:p-20">
+                    <img src={author.image.asset.url} alt={author.image.url} className="rounded w-32 h-32 mx-auto lg:w-64 lg:h-64 md:mr-8" />
+                    <div className="text-lg sm:flex flex-col justify-center relative mx-auto">
+                        <h1 className=" cursive text-2xl pt-5 sm:pt=0 md:text-4xl lg:text-6xl text-black mb-4">
                             Hello! I am {" "}
-                            <span className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-700 via-indigo-300 to-pink-200">{author.name}</span>
+                            <span className="text-2xl md:text-4xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-700 via-indigo-300 to-pink-200">{author.name}</span>
                         </h1>
                         <div className="prose lg:prose-xl flex text-black casual justify-center">
                             <BlockContent blocks={author.bio} projectId="370xn94r" dataset="production"/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
         <header className="bg-black">
             <div className="container mx-auto flex justify-between">
@@ -11,32 +12,35 @@ export default function NavBar() {
                     to="/" 
                     exact 
                     activeClassName="text-white"
-                    className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-white text-4xl font-bold cursive tracking-widest text-white">
+                    className="inline-flex items-center sm:py-6 px-3 mr-4 text-white hover:text-white text-1xl md:text-4xl font-bold cursive tracking-widest text-white">
                         AntonPodkur
                     </NavLink>
+                    <button className="text-white casual hidden">Menu</button>
+                    <div className="flex flex-col sm:flex-row ">
                     <NavLink 
                     to="/post" 
-                    className="inline-flex items-center py-3 px-3 my-6 text-white hover:text-gray-400 casual"
+                    className="inline-flex items-center sm:py-3 px-3 my-6 text-white hover:text-gray-400 casual"
                     activeClassName="text-red-100 bg-purple-700 rounded"
                     >
                         Blog
                     </NavLink>
                     <NavLink 
                     to="/project" 
-                    className="inline-flex items-center py-3 px-3 my-6 text-white hover:text-gray-400 casual"
+                    className="inline-flex items-center sm:py-3 px-3 my-6 text-white hover:text-gray-400 casual"
                     activeClassName="text-red-100 bg-purple-700 rounded"
                     >
                         Projects
                     </NavLink>
                     <NavLink 
                     to="/about" 
-                    className="inline-flex items-center py-3 px-3 my-6 text-white hover:text-gray-400 casual"
+                    className="inline-flex items-center sm:py-3 px-3 my-6 text-white hover:text-gray-400 casual"
                     activeClassName="text-red-100 bg-purple-700 rounded"
                     >
                         About me
                     </NavLink>
+                    </div>
                 </nav>
-                <div className="inline-flex py-3 px-3 my-6">
+                <div className="inline-flex sm:py-3 px-3 my-6">
                     <SocialIcon url="https://github.com/antonpodkur" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
                     <SocialIcon url="https://www.instagram.com/antonpodkur/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
                 </div>
