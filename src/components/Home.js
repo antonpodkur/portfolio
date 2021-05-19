a8cc25055cff2b8f265ec3dc0aeff4bd608c9230
Anton Podkur <antonpodkur@pop-os.localdomain>
2021-05-20 02:06:29 +0300

    animated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <main className="bg-gradient-to-br from-purple-400 via-indigo-300 to-pink-200">
             <section className="relative text-center flex-col justify-center min-h-screen pt-12 lg:pt-32 px-8">
-                <h1 className="text-7xl sm:text-9xl black font-bold cursive leading-none lg:leading-snug pt-10">Hello.</h1>
-                <h2 className="text-6xl sm:text-7xl black font-bold cursive leading-none lg:leading-snug pt-20">I am happy to see you here.</h2>
+                <h1 className="text-7xl sm:text-9xl black font-bold cursive leading-none lg:leading-snug pt-10 text-reveal-left">Hello.</h1>
+                <h2 className="text-6xl sm:text-7xl black font-bold cursive leading-none lg:leading-snug pt-20 text-reveal">I am happy to see you here.</h2>
             </section>
         </main>    
     )
